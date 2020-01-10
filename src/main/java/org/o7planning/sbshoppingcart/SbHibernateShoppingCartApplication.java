1a0347b550f94f5d5e9adb3589dc7f870f866a10
Xinyi Wang <xinyi.wang@cpwm.com>
2020-01-10 15:01:27 -0800

    first commit

---
+++ src/main/java/org/o7planning/sbshoppingcart/SbHibernateShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHibernateShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbHibernateShoppingCartApplication.class, args);
+	}
+
+}
